aec86ba2a05f665379f4e949016f173159dee057
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-12-14 16:41:08 -0700

    Headline component bare bones

---
+++ src/components/Headline/name-position.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NameTitle = () => {
+  return (
+    <div>
+      <div className="NameTitle-Name">Dillon Craw</div>
+      <div className="NameTitle-Underline"></div>
+      <div className="NameTitle-Title">Full Stack Web Developer</div>
+    </div>
+  )
+}
+
+export default NameTitle

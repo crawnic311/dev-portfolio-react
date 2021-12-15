ce71075a4917fdbbfb1cc488a7fa56f6adb7643e
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-12-15 14:34:01 -0700

    Icons Added

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -3,17 +3,78 @@ import React from 'react'
 const Skills = () => {
   return (
     <div>
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
-      Images in here
+      <div>
+        <img
+          src={require('../../skills-icons/JavaScript Icon.png')}
+          alt=""
+          className="Skills-icon"
+        />
+        JavaScript
+      </div>
+      <div>
+        <img
+          src={require('../../skills-icons/React Icon.png')}
+          alt=""
+          className="Skills-icon"
+        />
+        React
+      </div>
+      <div>
+        <img
+          src={require('../../skills-icons/Redux Icon.png')}
+          alt=""
+          className="Skills-icon"
+        />
+        Redux
+      </div>
+      <div>
+        <img
+          src={require('../../skills-icons/HTML Icon.png')}
+          alt=""
+          className="Skills-icon"
+        />
+        HTML
+      </div>
+      <div>
+        <img
+          src={require('../../skills-icons/CSS Icon.png')}
+          alt=""
+          className="Skills-icon"
+        />
+        CSS
+      </div>
+      <div>
+        <img
+          src={require('../../skills-icons/Express Icon.png')}
+          alt=""
+          className="Skills-icon"
+        />
+        Express
+      </div>
+      <div>
+        <img
+          src={require('../../skills-icons/PostgreSQL Icon.png')}
+          alt=""
+          className="Skills-icon"
+        />
+        PostgreSQL
+      </div>
+      <div>
+        <img
+          src={require('../../skills-icons/Node Icon.png')}
+          alt=""
+          className="Skills-icon"
+        />
+        Node
+      </div>
+      <div>
+        <img
+          src={require('../../skills-icons/C++ Icon.png')}
+          alt=""
+          className="Skills-icon"
+        />
+        C++
+      </div>
     </div>
   )
 }

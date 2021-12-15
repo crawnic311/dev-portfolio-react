310a3bb1635c24aae0979ebe699feb1d82b53e7b
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-12-15 14:43:33 -0700

    Contact Headline Component Built Bare Bones

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css'
 import Headline from './components/Headline/name-position'
 import AboutMe from './components/About-Me/about-me'
 import Skills from './components/Skills/skills'
+import ProjectHeadline from './components/Projects/projects-headline'
+import ContactHeadline from './components/Get-In-Touch/contact-headline'
 
 function App() {
   return (
@@ -15,6 +17,12 @@ function App() {
       </div>
       <div>
         <Skills />
+      </div>
+      <div>
+        <ProjectHeadline />
+      </div>
+      <div>
+        <ContactHeadline />
       </div>
     </div>
   )

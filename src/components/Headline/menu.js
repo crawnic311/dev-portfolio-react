54753f1f225da1183aa5e2db099395419ddad17f
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-12-16 18:42:49 -0700

    Modified Menu Position

--- src/components/Headline/menu.js
+++ src/components/Headline/menu.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Menu = () => {
   return (
     <div className="Headline-Menu">
-      <button>Menu</button>
+      <a className="Menu">
+        <span className="Menu-Text">Menu</span>
+      </a>
     </div>
   )
 }

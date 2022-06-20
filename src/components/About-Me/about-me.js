eead0a7cda2452cb1b415ccde5f22e20798c6a75
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2022-06-19 21:38:23 -0600

    WebDev Icon displaying

--- src/components/About-Me/about-me.js
+++ src/components/About-Me/about-me.js
@@ -3,11 +3,7 @@ import React from 'react'
 const AboutMe = () => {
   return (
     <div>
-      <img
-        src={require('//src/section-icons/webDev Icon.png')}
-        alt=""
-        className={styles.SkillsIcon}
-      />
+      <img src={require('../../section-icons/webDev Icon.png')} alt="" />
       It's no secret that I love to learn and challenge myself. After a few
       years working in operations I knew it was time for a new challenge. I
       followed my intuition and enrolled in a web dev bootcamp. Since then I

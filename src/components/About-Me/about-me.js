ea479baaacb29e3aa61179d830ded3e1fa38f32b
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2022-06-22 18:32:57 -0600

    Changing About me BIO

--- src/components/About-Me/about-me.js
+++ src/components/About-Me/about-me.js
@@ -8,10 +8,10 @@ const AboutMe = () => {
         alt=""
         className="About-Me-Icon"
       />
-      It's no secret that I love to learn and challenge myself. After a few
-      years working in operations I knew it was time for a new challenge. I
-      followed my intuition and enrolled in a web dev bootcamp. Since then I
-      have been enjoying bringing my creative ideas into exisistence.
+      It's no secret that I love to learn and push myself. After a few years
+      working in operations I knew it was time for a new challenge. I followed
+      my intuition and enrolled in a web dev bootcamp. Since then I have been
+      enjoying bringing my creative ideas into exisistence.
       <br></br>
       <br></br>I like the outdoors, the indoors, excellent foods, and keeping up
       with each new installment of the Legend of Zelda series. I've lived all

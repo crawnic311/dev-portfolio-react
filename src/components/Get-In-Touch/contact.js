dda132d16c4297c97dc7302b5c2d35e567fb517b
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2022-06-24 18:28:01 -0600

    Classes and Id's for contact buttons

--- src/components/Get-In-Touch/contact.js
+++ src/components/Get-In-Touch/contact.js
@@ -10,17 +10,29 @@ const Contact = () => {
           <a
             href="https://docs.google.com/document/d/1E6UPWmel1o5LDudCTl6ecdWPIph7kBgzAJEWmtAK6N4/edit"
             target="_blank"
+            className="Contact-Button"
+            id="Resume-Button"
           >
             Resume
           </a>
         </button>
         <button>
-          <a href="https://www.linkedin.com/in/dillon-craw" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/dillon-craw"
+            target="_blank"
+            className="Contact-Button"
+            id="LinkedIn-Button"
+          >
             LinkedIn
           </a>
         </button>
         <button>
-          <a href="https://github.com/crawnic311" target="_blank">
+          <a
+            href="https://github.com/crawnic311"
+            target="_blank"
+            className="Contact-Button"
+            id="Github-Button"
+          >
             Github
           </a>
         </button>

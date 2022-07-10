09ae10dedd4a564ce5f6e66c5be4d48070926689
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2022-07-09 22:21:05 -0800

    Angled Sections Started

--- src/App.js
+++ src/App.js
@@ -22,19 +22,20 @@ function App() {
       <div className="App-Headline-Section" id="Headline-BG-Image">
         <Headline />
       </div>
-      <div className="Sections-Wrapper-Below-Header">
+      <div className="Gradient-Wrapper-AboutMe">
         <div className="App-AboutMe-Section">
           <AboutMeHeadline />
           <AboutMe />
         </div>
-        <div className="App-Skills-Section">
-          <SkillsHeadline />
-          <Skills />
-        </div>
-        <div className="App-Projects-Section">
-          <ProjectHeadline />
-        </div>
       </div>
+      <div className="App-Skills-Section">
+        <SkillsHeadline />
+        <Skills />
+      </div>
+      <div className="App-Projects-Section">
+        <ProjectHeadline />
+      </div>
+
       <div className="App-Contact-Section">
         <ContactHeadline />
         <Contact />

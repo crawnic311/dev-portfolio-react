fe516c7f0ab992add6c04081b54ff43cfdd2a23c
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2022-07-19 10:30:38 -0600

    MenuToggle Component Created

---
+++ src/components/Menu/menu-toggle.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const MenuToggle = () => {
+  return (
+    <div className="Is-Menu-Visilbe">
+      <nav>
+        <div>
+          <h2></h2>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <a></a>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default MenuToggle
